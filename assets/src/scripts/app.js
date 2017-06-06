f7031d5694f870b2d12f30443c3a0c67cdc27485
trevorsee <tcarr@mica.edu>
2017-06-06 11:08:31 -0400

    add swiper

--- assets/src/scripts/app.js
+++ assets/src/scripts/app.js
@@ -2,3 +2,12 @@
  * The main entry point into the site. This file gets transpiled
  * and bundled using Webpack.
  */
+
+ var mySwiper = new Swiper ('.swiper-container', {
+   direction: 'vertical',
+   nextButton: '.swiper-button-next',
+   prevButton: '.swiper-button-prev',
+   spaceBetween: 30,
+   hashnav: true,
+   hashnavWatchState: true
+ })
