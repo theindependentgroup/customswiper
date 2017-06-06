e2cbd01e277a068e13ff9df92ff86a874aca8ce9
trevorsee <tcarr@mica.edu>
2017-06-06 12:25:11 -0400

    two axis swipers

--- assets/src/scripts/app.js
+++ assets/src/scripts/app.js
@@ -3,11 +3,38 @@
  * and bundled using Webpack.
  */
 
- var mySwiper = new Swiper ('.swiper-container', {
-   direction: 'vertical',
-   nextButton: '.swiper-button-next',
-   prevButton: '.swiper-button-prev',
-   spaceBetween: 30,
-   hashnav: true,
-   hashnavWatchState: true
- })
+var mainSwiper = new Swiper ('.swiper-projects', {
+  direction: 'vertical',
+  //  nextButton: '.swiper-button-next',
+  //  prevButton: '.swiper-button-prev',
+  keyboardControl: true,
+  hashnav: true,
+  hashnavWatchState: true
+})
+
+
+// iterate through each project and initiate Swipers
+var main = document.getElementsByClassName('swiper-projects');
+var eachProj = main[0].children[0].children;
+for (var i = 0; i < eachProj.length; i++) {
+   var classes = eachProj[i].children[0].classList;
+   var name = classes[0]
+   var cleanName = name.replace(/-/g,"");
+
+
+   window[cleanName + "Swiper"] = new Swiper ('.'+name, {
+     keyboardControl: true
+   });
+   console.log(name);
+   console.log(window[cleanName + "Swiper"]);
+
+  //  var mainSwiper = new Swiper ('.swiper-projects', {
+  //    direction: 'vertical',
+  //    //  nextButton: '.swiper-button-next',
+  //    //  prevButton: '.swiper-button-prev',
+  //    keyboardControl: true,
+  //    hashnav: true,
+  //    hashnavWatchState: true
+  // })
+
+}
