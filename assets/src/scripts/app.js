46719cc31a386ddf3d51b3fa6f5d3e18e3e6f6ad
trevorsee <tcarr@mica.edu>
2017-06-06 20:50:39 -0400

    glitchy sliders connected

--- assets/src/scripts/app.js
+++ assets/src/scripts/app.js
@@ -21,20 +21,68 @@ for (var i = 0; i < eachProj.length; i++) {
    var name = classes[0]
    var cleanName = name.replace(/-/g,"");
 
-
    window[cleanName + "Swiper"] = new Swiper ('.'+name, {
-     keyboardControl: true
+     keyboardControl: true,
+     pagination: '.swiper-pagination',
+     paginationType: 'fraction',
+     loop: true
    });
-   console.log(name);
-   console.log(window[cleanName + "Swiper"]);
+   //console.log(cleanName);
+   //console.log(name);
+   console.log( eval(cleanName + "Swiper") );
 
-  //  var mainSwiper = new Swiper ('.swiper-projects', {
-  //    direction: 'vertical',
-  //    //  nextButton: '.swiper-button-next',
-  //    //  prevButton: '.swiper-button-prev',
-  //    keyboardControl: true,
-  //    hashnav: true,
-  //    hashnavWatchState: true
-  // })
+  //  eval(cleanName + "Swiper.on('onSlideNextStart', function () { \
+  //    var test = " + cleanName + "Swiper.isEnd; \
+  //    console.log(test); \
+  //  })");
+  //
+  eval(cleanName + "Swiper.on('onSlideNextStart', function () { \
+    var test = " + cleanName + "Swiper.isEnd; \
+    if (test) { \
+      mainSwiper.slideNext(false); \
+      resetAllNe(); \
+      console.log(test); \
+    } \
+  })");
 
+  eval(cleanName + "Swiper.on('onSlidePrevStart', function () { \
+    var test = " + cleanName + "Swiper.isBeginning; \
+    if (test) { \
+      mainSwiper.slidePrev(false); \
+      resetAllPr(); \
+      console.log(test); \
+    } \
+  })");
+
+  // window[cleanName + "Swiper"].on('onSlideNextStart', function () {
+  //   var test = window[cleanName + "Swiper"].container;
+  //   console.log(test);
+  //  });
+}
+
+
+var resetAllPr = function() {
+  var main = document.getElementsByClassName('swiper-projects');
+  var eachProj = main[0].children[0].children;
+  for (var i = 0; i < eachProj.length; i++) {
+     var classes = eachProj[i].children[0].classList;
+     var name = classes[0]
+     var cleanName = name.replace(/-/g,"");
+
+     eval(cleanName + "Swiper.slideTo(0,1,0)" );
+
+   }
+}
+
+var resetAllNe = function() {
+  var main = document.getElementsByClassName('swiper-projects');
+  var eachProj = main[0].children[0].children;
+  for (var i = 0; i < eachProj.length; i++) {
+     var classes = eachProj[i].children[0].classList;
+     var name = classes[0]
+     var cleanName = name.replace(/-/g,"");
+
+     eval(cleanName + "Swiper.slideTo(1,1,0)" );
+
+   }
 }
